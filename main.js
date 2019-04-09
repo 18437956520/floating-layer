af600db1171e5625f8811baf41a0a252ef594da3
yyyh <444510796@qq.com>
2019-04-09 12:04:36 +0800

    more 节省内存

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $(click).on('click', function(){
     $(popover).show()
+
+    $(document).one('click', function(){
+        $(popover).hide()
+        console.log(1)
+    })
 })
 
 $(wrapper).on('click', function(x){
     x.stopPropagation()
 })
 
-$(document).on('click', function(){
-    $(popover).hide()
-})
