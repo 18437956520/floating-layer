7278661f86cfefacb5c15a0c5c70246b77e0394c
yyyh <444510796@qq.com>
2019-04-09 11:35:05 +0800

    踩坑

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 click.addEventListener('click', function(){
     popover.style.display = 'block'
+    console.log(1)
+})
+
+document.addEventListener('click', function(){
+    popover.style.display = 'none'
+    console.log(2)
 })
